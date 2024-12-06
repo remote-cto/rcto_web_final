4f5c5df1bc93c470b657fc9c98467ccff9c53cc0
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2024-12-06 14:09:12 +0530

    chnages done

--- app/components/BannerPage.tsx
+++ app/components/BannerPage.tsx
@@ -78,7 +78,7 @@ const BannerPage = () => {
                   onClick={scrollToLearnMore}
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-xl rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
-                  Learn More
+                  Get Started
                 </button>
               </CoolMode>
             </motion.div>
