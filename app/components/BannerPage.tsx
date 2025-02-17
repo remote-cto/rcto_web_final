09b5d3475167a9123805f9f4a8288b1a00cfdfa2
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-02-17 22:58:35 +0530

    text added

--- app/components/BannerPage.tsx
+++ app/components/BannerPage.tsx
@@ -284,7 +284,7 @@ const BannerPage = ({
                   transition={{ duration: 0.8, delay: 0.2 }}
                 >
                   <span
-                    className="inline-block whitespace-nowrap font-['Merriweather'] mb-2 lg:mb-5 text-white"
+                    className="inline-block whitespace-nowrap font-['Montserrat'] mb-2 lg:mb-5 text-white"
                     // style={{
                     //   textShadow:
                     //     "0 0 5px rgba(255,255,255,0.8), 0 0 10px rgba(255,255,255,0.5), 0 0 15px rgba(255,255,255,0.3)",
@@ -302,7 +302,7 @@ const BannerPage = ({
                     TECH
                   </span>{" "}
                   <span
-                    className="inline-block whitespace-nowrap text-white font-['Merriweather'] mt-1 lg:mt-3"
+                    className="inline-block whitespace-nowrap text-white font-['Montserrat'] mt-1 lg:mt-3"
                     // style={{
                     //   textShadow:
                     //     "0 0 5px rgba(255,255,255,0.8), 0 0 10px rgba(255,255,255,0.5), 0 0 15px rgba(255,255,255,0.3)",
